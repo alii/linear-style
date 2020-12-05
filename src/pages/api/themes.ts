41356126a677cbf0e310b88a69cf1b2db08e591e
Alistair Smith <alistairsmith01@gmail.com>
2020-12-05 00:04:37 +0000

    fix: Update the midnight theme

--- src/pages/api/themes.ts
+++ src/pages/api/themes.ts
@@ -2,7 +2,7 @@ import { NowRequest, NowResponse } from "@vercel/node";
 
 const themes: Record<string, string> = {
   "Tokyo Night": "#17161F,#CCCCCC,#1A1A27,#5C5C87,#61D0FF,#17161F",
-  "Midnight": "#17161F,#CCCCCC,#1A1A27,#5C5C87,#61D0FF,#17161F",
+  "Midnight": "#0F0F10,#EEEFF1,#151516,#EEEFF1,#D25E65,#FFFFFF",
   "Dawn": "#2A222E,#EEEFF1,#382A3C,#EEEFF1,#A84376,#FFFFFF",
   "Pale": "#292D3E,#EEEFF1,#292D3E,#EEEFF1,#7D57C1,#FFFFFF",
   "Barbie Dreamhouse": "#E2DAF1,#593E74,#FCDEEE,#593E74,#B8FAFA,#8B6BC7",
