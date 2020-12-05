52d22cae355493b128c6ddce6c5cf4457dc60518
Juan Garcia <37905409+fangskillz@users.noreply.github.com>
2020-12-05 00:56:30 -0600

    refactor(ThemeCard/Navbar): Increases Scale on hover. Navbar links transition color on hover

--- src/molecules/Navbar.tsx
+++ src/molecules/Navbar.tsx
@@ -24,6 +24,7 @@ export const StyledNavbar = styled.div`
     color: ${(props) => props.theme.colors.text}5c;
     align-items: center;
     margin-right: 25px;
+    transition: 200ms ease-out 0s;
 
     &:hover {
       color: ${(props) => props.theme.colors.text}ff;
