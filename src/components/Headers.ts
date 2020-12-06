6e61ed3f09060445f0da963820a5a31f5337eaeb
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-06 17:32:27 -0600

    refactor(core): Extract

---
+++ src/components/Headers.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 0.4em;
+  font-weight: 800;
+  font-size: 65px;
+  color: white;
+`;
+
+export const Subtitle = styled.span`
+  text-align: center;
+  margin-bottom: 20px;
+  color: #8a8f98;
+  font-weight: 500;
+  font-size: 22px;
+`;
+
+export const LearnMore = styled.h3`
+  text-align: center;
+  margin-bottom: 50px;
+  color: #8a8f98;
+  font-weight: 500;
+  font-size: 18px;
+`;
+
+export const Link = styled.a`
+  color: #5d6ad0;
+  text-decoration: none;
+`;
