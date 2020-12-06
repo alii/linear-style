7f99e1ae5b8c4a790d9f8f100fee98b9dd8c8ec2
Alistair Smith <alistairsmith01@gmail.com>
2020-12-06 23:57:15 +0000

    fix: clean up globalstyle in favor of component abstractions

--- src/components/SearchInput.ts
+++ src/components/SearchInput.ts
@@ -3,4 +3,18 @@ import styled from "styled-components";
 export const SearchInput = styled.input`
   margin-top: 5px;
   outline: none;
+  margin-bottom: 25px;
+  padding: 10px 20px;
+  background: transparent;
+  border: 3px solid ${props => props.theme.colors.lighterBackground};
+  border-radius: 5px;
+  font-family: "Inter var", sans-serif;
+  color: ${props => props.theme.colors.text};
+`;
+
+export const SearchLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  color: ${props => props.theme.colors.text}6c;
+  font-size: 12px;
 `;
