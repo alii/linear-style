88b8fc901958971d4fb06f63dcbc39868281e43b
Alistair Smith <alistairsmith01@gmail.com>
2020-12-07 01:45:53 +0000

    fix: default to empty arr
    footgun

--- src/core/hooks.ts
+++ src/core/hooks.ts
@@ -5,7 +5,7 @@ export function useLinearThemes() {
   return useSWR<Record<string, string>>("/api/themes");
 }
 
-export function useInputFilter<T>(predicate: (item: T, index: number, state: string) => boolean, items?: T[]) {
+export function useInputFilter<T>(predicate: (item: T, index: number, state: string) => boolean, items: T[] = []) {
   const [state, setState] = useState("");
 
   const filtered = state.trim() === "" ? items : items.filter((item, index) => predicate(item, index, state));
