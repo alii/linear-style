7250ee8bcf7f24560c01e99a12830fc67f39d24d
Alistair Smith <alistairsmith01@gmail.com>
2021-11-30 09:42:23 -0800

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { AppProps } from "next/app";
 import { appTheme } from "../core/app-themes";
 import { GlobalStyle } from "../components/GlobalStyle";
 import { Navbar } from "../molecules/Navbar";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,17 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Linear Style</title>
       </Head>
+
+      <Script src="https://lab.alistair.cloud/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+
       <ThemeProvider theme={appTheme}>
         <GlobalStyle />
         <Navbar />
